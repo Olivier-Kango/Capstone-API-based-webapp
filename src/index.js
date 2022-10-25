a02e3924097914ab5b877b6d3162582b68062cf9
Olivier-Kango <olivierkango@gmail.com>
2022-10-25 16:09:45 -0700

    Set up the Webpack for images

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
 import './style.css';
+import Icon from './icon.png'
--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,10 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
 };
